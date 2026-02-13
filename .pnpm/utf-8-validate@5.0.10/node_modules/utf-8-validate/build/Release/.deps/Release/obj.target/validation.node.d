6ab2f64b7a38b2ef47b4c77661c233e7d6237301
scpcom <scpcom@gmx.de>
2026-02-13 01:13:10 +0100

    Initial commit

---
+++ .pnpm/utf-8-validate@5.0.10/node_modules/utf-8-validate/build/Release/.deps/Release/obj.target/validation.node.d
@@ -0,0 +1,1 @@
+cmd_Release/obj.target/validation.node := /mnt/N2/Driver/LicheeRV-Nano/LicheeSG-Nano-Build/buildroot/output/cvitek_SG200X_musl_riscv64/per-package/nanokvm-server/host/bin/ccache /usr/bin/g++ -o Release/obj.target/validation.node -shared -pthread -rdynamic -m64  -Wl,-soname=validation.node -Wl,--start-group Release/obj.target/validation/src/validation.o -Wl,--end-group 
